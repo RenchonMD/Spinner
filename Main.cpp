864505a42a38846695fd2e3ffaaa8f498b6f9abd
Renchon <92147511+RenchonMD@users.noreply.github.com>
2021-11-22 19:01:20 +0900

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,176 @@
+#include <list>
+#include <vector>
+#include <string.h>
+#include <pthread.h>
+#include <cstring>
+#include <jni.h>
+#include <unistd.h>
+#include <fstream>
+#include <iostream>
+#include <dlfcn.h>
+#include "Includes/Logger.h"
+#include "Includes/obfuscate.h"
+#include "Includes/Utils.h"
+
+#include "KittyMemory/MemoryPatch.h"
+#include "Menu.h"
+
+//Target lib here
+#define targetLibName OBFUSCATE("libFileA.so")
+
+#include "Includes/Macros.h"
+
+// fancy struct for patches for kittyMemory
+struct My_Patches {
+    // let's assume we have patches for these functions for whatever game
+    // like show in miniMap boolean function
+    MemoryPatch SliderExample;
+    // etc...
+} hexPatches;
+
+bool feature1, feature2, featureHookToggle, Health;
+int sliderValue = 1, level = 0;
+void *instanceBtn;
+
+// Hooking examples. Assuming you know how to write hook
+
+// we will run our hacks in a new thread so our while loop doesn't block process main thread
+void *hack_thread(void *) {
+    LOGI(OBFUSCATE("pthread created"));
+
+    //Check if target lib is loaded
+    do {
+        sleep(1);
+    } while (!isLibraryLoaded(targetLibName));
+
+    //Anti-lib rename
+    /*
+    do {
+        sleep(1);
+    } while (!isLibraryLoaded("libYOURNAME.so"));*/
+
+    LOGI(OBFUSCATE("%s has been loaded"), (const char *) targetLibName);
+
+#if defined(__aarch64__) 
+
+#else 
+
+    LOGI(OBFUSCATE("Done"));
+#endif
+
+    return NULL;
+}
+
+//JNI calls
+extern "C" {
+
+// Do not change or translate the first text unless you know what you are doing
+// Assigning feature numbers is optional. Without it, it will automatically count for you, starting from 0
+// Assigned feature numbers can be like any numbers 1,3,200,10... instead in order 0,1,2,3,4,5...
+// ButtonLink, Category, RichTextView and RichWebView is not counted. They can't have feature number assigned
+// Toggle, ButtonOnOff and Checkbox can be switched on by default, if you add True_. Example: CheckBox_True_The Check Box
+// To learn HTML, go to this page: https://www.w3schools.com/
+
+JNIEXPORT jobjectArray
+JNICALL
+Java_uk_lgl_modmenu_FloatingModMenuService_getFeatureList(JNIEnv *env, jobject context) {
+    jobjectArray ret;
+
+    //Toasts added here so it's harder to remove it
+    MakeToast(env, context, OBFUSCATE("Modded by LGL"), Toast::LENGTH_LONG);
+
+    const char *features[] = {    
+            OBFUSCATE("0_Spinner_The spinner_Items 1,Items 2,Items 3"),           
+    };
+
+    //Now you dont have to manually update the number everytime;
+    int Total_Feature = (sizeof features / sizeof features[0]);
+    ret = (jobjectArray)
+            env->NewObjectArray(Total_Feature, env->FindClass(OBFUSCATE("java/lang/String")),
+                                env->NewStringUTF(""));
+
+    for (int i = 0; i < Total_Feature; i++)
+        env->SetObjectArrayElement(ret, i, env->NewStringUTF(features[i]));
+
+    pthread_t ptid;
+    pthread_create(&ptid, NULL, antiLeech, NULL);
+
+    return (ret);
+}
+
+JNIEXPORT void JNICALL
+Java_uk_lgl_modmenu_Preferences_Changes(JNIEnv *env, jclass clazz, jobject obj,
+                                        jint featNum, jstring featName, jint value,
+                                        jboolean boolean, jstring str) {
+
+    LOGD(OBFUSCATE("Feature name: %d - %s | Value: = %d | Bool: = %d | Text: = %s"), featNum,
+         env->GetStringUTFChars(featName, 0), value,
+         boolean, str != NULL ? env->GetStringUTFChars(str, 0) : "");
+
+    //BE CAREFUL NOT TO ACCIDENTLY REMOVE break;
+
+    switch (featNum) {
+        case 0:
+            switch (value) {
+                //For noobies
+                case 0:
+                    hexPatches.SliderExample = MemoryPatch::createWithHex(
+                            targetLibName, string2Offset(
+                                    OBFUSCATE("0x100000")),
+                            OBFUSCATE(
+                                    "00 00 A0 E3 1E FF 2F E1"));
+                    hexPatches.SliderExample.Modify();
+                    break;
+                case 1:
+                    hexPatches.SliderExample = MemoryPatch::createWithHex(
+                            targetLibName, string2Offset(
+                                    OBFUSCATE("0x100000")),
+                            OBFUSCATE(
+                                    "01 00 A0 E3 1E FF 2F E1"));
+                    hexPatches.SliderExample.Modify();
+                    break;
+                case 2:
+                    hexPatches.SliderExample = MemoryPatch::createWithHex(
+                            targetLibName,
+                            string2Offset(
+                                    OBFUSCATE("0x100000")),
+                            OBFUSCATE(
+                                    "02 00 A0 E3 1E FF 2F E1"));
+                    hexPatches.SliderExample.Modify();
+                    break;
+            }
+            break;
+        case 1:
+            switch (value) {
+                case 0:
+                    LOGD(OBFUSCATE("Selected item 1"));
+                    break;
+                case 1:
+                    LOGD(OBFUSCATE("Selected item 2"));
+                    break;
+                case 2:
+                    LOGD(OBFUSCATE("Selected item 3"));
+                    break;
+            }
+            break;      
+    }
+}
+}
+
+//No need to use JNI_OnLoad, since we don't use JNIEnv
+//We do this to hide OnLoad from disassembler
+__attribute__((constructor))
+void lib_main() {
+    // Create a new thread so it does not block the main thread, means the game would not freeze
+    pthread_t ptid;
+    pthread_create(&ptid, NULL, hack_thread, NULL);
+}
+
+/*
+JNIEXPORT jint JNICALL
+JNI_OnLoad(JavaVM *vm, void *reserved) {
+    JNIEnv *globalEnv;
+    vm->GetEnv((void **) &globalEnv, JNI_VERSION_1_6);
+    return JNI_VERSION_1_6;
+}
+ */
